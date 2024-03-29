de92931a4ce3fd4212671bf998fdbedb6d7f303f
weijie-lim <47061871+weijie-lim@users.noreply.github.com>
2024-03-29 05:38:09 +0000

    updated UI with error handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ import altair as alt
 import time
 import zipfile
 import pandas as pd
-from datetime import datetime
+import datetime
 import re
 
 # Page title
@@ -42,32 +42,60 @@ with st.sidebar:
         url_regex = re.compile(r'https?://(?:www\.)?[a-zA-Z0-9./]+')
         return bool(url_regex.match(url))
 
+    def is_valid_datestring(date_text):
+        try:
+            datetime.date.fromisoformat(date_text)
+        except ValueError:
+            return False
+        return True
+
     st.header('Upload and Submit your Questions!')
     st.markdown('**1a. Input the URL of Log Files**')
     number_inputs = st.number_input('number of URLs', step=1, min_value=1)
     st.write('number of URLs to upload ', number_inputs)
 
-    data = pd.DataFrame(columns=['nr'])
-
-    if 'df' not in st.session_state:
-        st.session_state.df = data
-
     input_values = [st.text_input(f'URL No. {i+1}', '', key=f"text_input_{i}")
-          for i in range(number_inputs)]
-    # uploaded_files = st.file_uploader("Choose a TXT file", accept_multiple_files=True,
-    #                                     type=['txt'])
-    # for uploaded_file in uploaded_files:
-    #     bytes_data = uploaded_file.read()
-    #     st.write("filename:", uploaded_file.name)
-    #     st.write(bytes_data)
+    for i in range(number_inputs)]
     
-    st.markdown('**1b. Input Your Question**')
-    input_question = st.text_input("Ask your question", key="input_question")
+    for link in input_values:
+        if is_valid_url(link):
+            date_str =link.split('/')[-1].split('_')[-2]
+            if not is_valid_datestring(date_str):
+                st.write(f"Check date format in Url, follow YYYYMMDD format")
+    
+    st.markdown('**1b. Input Your Question**')  
+    input_question = st.text_input("Ask your question")
 
     st.markdown('**1c. Select Date of Logs to Read From**')
+    list_of_dates = []
+    date_format = '%Y%m%d'
+    now = datetime.datetime.now()
+    if "least_recent" not in st.session_state:
+        st.session_state.least_recent =now
+    if "most_recent" not in st.session_state:
+        st.session_state.most_recent = None
+
+    for link in input_values:
+        if is_valid_url(link):
+            date_str =link.split('/')[-1].split('_')[-2]
+            if is_valid_datestring(date_str):
+                date_obj = datetime.datetime.strptime(date_str, date_format)
+                list_of_dates.append(date_obj)
+                if date_obj < st.session_state.least_recent:
+                    st.session_state.least_recent = date_obj
+                      
+    if len(list_of_dates) > 1:
+        if st.session_state.most_recent is None:
+            st.session_state.most_recent = datetime.datetime(1900,1,1)
+
+        for i, d in enumerate(list_of_dates):
+            if d > st.session_state.most_recent:
+                st.session_state.most_recent = d
+
     end_time = st.slider(
         "When do you start?",
-        min_value=datetime(2019, 1, 1),
+        min_value=st.session_state.least_recent,
+        max_value=st.session_state.most_recent,
         # value=datetime(2020, 1, 1),
         format="YYYY/MM/DD")
 
@@ -85,13 +113,17 @@ with st.sidebar:
                 if not is_valid_url(url):
                     st.write(f"URL No. {i+1} is invalid!!!\r...")
                     can_continue = False
+                else:
+                    date_str =link.split('/')[-1].split('_')[-2]
+                    if not is_valid_datestring(date_str):
+                        can_continue = False
+                        st.write(f"Check date format in Url, follow YYYYMMDD")
+
 
         # update dataframe state
         if can_continue:
             #check for datetime
             
-
-
             to_upload = {
                 "question": input_question,
                 "documents": input_values,
@@ -99,6 +131,7 @@ with st.sidebar:
             }
 
             st.text(to_upload)
+
 
 
 
